6b4ea29e31aeac0bb8f9624b500e18bda6acdc99
jamieberrier <berrier.jamie@gmail.com>
2018-08-06 02:05:00 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -60,7 +60,7 @@ def all_winter_holiday_supplies(holiday_hash)
   holiday_hash.each do |season, season_supply_hash|
     if season == :winter
       season_supply_hash.each do |holiday, supplies_array|
-        supplies_array
+        winter_supplies = supplies_array
       end
     end
   end
