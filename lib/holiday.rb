03389a61449af7da4b9c9ab248d2833cfa2d74f3
jamieberrier <berrier.jamie@gmail.com>
2018-08-06 02:26:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -80,7 +80,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |season, season_supply_hash|
-    
+    puts "#{season}:"
   end
 end
 
