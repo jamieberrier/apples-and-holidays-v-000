356e7399c0fb1e15eeb98b726eb5477136b1d21f
jamieberrier <berrier.jamie@gmail.com>
2018-08-06 02:42:18 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -83,6 +83,7 @@ def all_supplies_in_holidays(holiday_hash)
     puts "#{season.capitalize}:"
     season_supply_hash.collect do |holiday, supplies_array|
       holiday_string = holiday.to_s
+      capitalize_array = []
       puts "  #{holiday_string.capitalize}: #{supplies_array.join(", ")}"
     end
   end
