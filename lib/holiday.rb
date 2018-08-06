f652c0ff3e3a95db32934d82bcb53caaa410f552
jamieberrier <berrier.jamie@gmail.com>
2018-08-06 01:24:42 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -28,7 +28,7 @@ def add_supply_to_winter_holidays(holiday_hash, supply)
   # Christmas AND the New Year's arrays
   holiday_hash.each do |season, supply_hash|
     if season == :winter
-
+      
     end
   end
 end
